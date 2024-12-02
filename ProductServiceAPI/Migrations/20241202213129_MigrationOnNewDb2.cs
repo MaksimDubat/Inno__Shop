8db00d386eb9aa1c45c2649e5c91b97524524c63
dubatovka.maksim <dubatovvka.maksim@gmail.com>
2024-12-03 00:35:16 +0300

    Migration on new db

---
+++ ProductServiceAPI/Migrations/20241202213129_MigrationOnNewDb2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductServiceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationOnNewDb2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
