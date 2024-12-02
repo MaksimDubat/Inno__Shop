d26c5e748c37fc8e0617b3fe9f7d99e822b3dfb8
dubatovka.maksim <dubatovvka.maksim@gmail.com>
2024-12-03 01:02:45 +0300

    tests

---
+++ UserControllerTests/UserControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserServiceAPI.Controllers;
+using UserServiceAPI.Entities;
+using UserServiceAPI.Interface;
+using Xunit;
+
+namespace UserAPITests
+{
+    public class UserControllerTests
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly UsersController _controller;
+
+        public UserControllerTests()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _controller = new UsersController(_userRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_UserExists_ReturnsOkResult()
+        {
+            // Arrange
+            var userId = 1;
+            var user = new AppUsers { Id = userId, Name = "John Doe" };
+            _userRepositoryMock.Setup(repo => repo.GetAsync(userId, It.IsAny<CancellationToken>()))
+                               .ReturnsAsync(user);
+
+            // Act
+            var result = await _controller.GetUserById(userId, CancellationToken.None);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedUser = Assert.IsType<AppUsers>(okResult.Value);
+            Assert.Equals(userId, returnedUser.Id);
+        }
+
+        [Fact]
+        public async Task GetById_UserDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            var userId = 1;
+            _userRepositoryMock.Setup(repo => repo.GetAsync(userId, It.IsAny<CancellationToken>()))
+                               .ReturnsAsync((AppUsers)null);
+
+            // Act
+            var result = await _controller.GetUserById(userId, CancellationToken.None);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+    }
+}
---
+++ UserControllerTests/UserRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserServiceAPI.DataBaseAccess;
+using UserServiceAPI.Entities;
+using UserServiceAPI.Infrastructure.Common;
+using Xunit;
+
+namespace UserAPITests
+{
+    public class UserRepositoryTests
+    {
+        private readonly MutableInnoShopDbContext _context;
+        private readonly UserRepository _repository;
+
+        public UserRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<MutableInnoShopDbContext>()
+                .UseNpgsql("UserTestDb")
+                .Options;
+
+            _context = new MutableInnoShopDbContext(options);
+            _repository = new UserRepository(_context);
+        }
+
+        [Fact]
+        public async Task GetAsync_UserExists_ReturnsUser()
+        {
+            // Arrange
+            var user = new AppUsers { Id = 1, Name = "John Doe" };
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            // Act
+            var result = await _repository.GetAsync(user.Id, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.Equals(user.Id, result.Id);
+        }
+    }
+}
